a29d9d03b41adb7fd8e054fb5ca70ad820a065e2
Mounir Raki <moraki@student.ethz.ch>
2024-06-25 16:47:59 +0200

    Add UNet implementation + constants and metrics files in utils

---
+++ utils/metrics.py
@@ -0,0 +1,14 @@
+import torch
+from constants import PATCH_SIZE, CUTOFF
+
+def patch_accuracy_fn(y_hat: torch.Tensor, y: torch.Tensor):
+    # computes accuracy weighted by patches (metric used on Kaggle for evaluation)
+    h_patches = y.shape[-2] // PATCH_SIZE
+    w_patches = y.shape[-1] // PATCH_SIZE
+    patches_hat = y_hat.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
+    patches = y.reshape(-1, 1, h_patches, PATCH_SIZE, w_patches, PATCH_SIZE).mean((-1, -3)) > CUTOFF
+    return (patches == patches_hat).float().mean()
+
+# TODO: Implement this
+def f1_score():
+    ...
---
+++ utils/models/UNet.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+
+# Implements a block of convolution (i.e. 2*(3x3 convolution + ReLU))
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Block, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(out_ch),
+            nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, padding=1),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, channels=(3,64,128,256,512,1024)):
+        super(UNet, self).__init__()
+        enc_chs = channels
+        dec_chs = channels[::-1][:-1] # Leave out the last value of the channels after flipping the list, since it is the number of channels of the final output
+
+        # Encoder blocks (using the Block class)
+        self.enc_blocks = nn.ModuleList([
+            Block(in_ch, out_ch) for in_ch, out_ch in zip(enc_chs[:-1], enc_chs[:1])
+        ])
+        # 2x2 max pooling (down-sampling the data)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+
+        # For the up-convolutions (i.e. fractionally strided convolutions, upsampling the data)
+        self.up_convs = nn.ModuleList([
+            nn.ConvTranspose2d(in_channels=in_ch, out_channels=out_ch, kernel_size=2, stride=2) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])
+        ])
+        # Decoder blocks (using the Block class)
+        self.dec_blocks = nn.ModuleList([
+            Block(in_ch, out_ch) for in_ch, out_ch in zip(dec_chs[:-1], dec_chs[1:])
+        ])
+        # 1x1 convolution to produce the output (using the last value of the flipped channel list as input dimension)
+        self.head = nn.Sequential(
+            nn.Conv2d(in_channels=dec_chs[-1], out_channels=1, kernel_size=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        enc_features = [] # To be able to implement the skip connections at decoding
+
+        # Encoder part
+        for block in self.enc_blocks[:-1]:
+            x = block(x)
+            enc_features.append(x) # Keep track of the encoded features at each step for the skip connections
+            x = self.pool(x) # Decrease resolution
+        x = self.enc_blocks[-1](x) # Latent space encoding
+
+        # Decoder part
+        for block, upconv, feature in zip(self.dec_blocks, self.up_convs, enc_features[::-1]):
+            x = upconv(x) # Increase resolution
+            x = torch.cat([x, feature], dim=1) # Concatenate skip features
+            x = block(x)
+
+        return self.head(x) # Reduce to 1 channel
