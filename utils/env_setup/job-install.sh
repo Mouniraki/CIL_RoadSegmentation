09efd3e0850a0f947a83f81abb544d4c43b39cd9
Mounir Raki <mouniraki@protonmail.com>
2024-07-14 12:39:07 +0200

    Adding bash scripts for student cluster

---
+++ utils/env_setup/job-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --account=cil
+#SBATCH --nodes=1                  # 24 cores
+#SBATCH --gpus=1
+###SBATCH --gres=gpumem:24g
+#SBATCH --time 00:30:00        ### adapt to our needs
+#SBATCH --mem-per-cpu=12000
+###SBATCH -J analysis1
+#SBATCH -o installation%j.out
+#SBATCH -e installation%j.err
+###SBATCH --mail-type=END,FAIL
+
+. /etc/profile.d/modules.sh
+
+echo "working"
+
+python3 -m venv /home/moraki/env-cil
+
+source /home/moraki/env-cil/bin/activate
+
+cd /home/moraki/CIL_RoadSegmentation
+
+#### put python commands here
+
+pip install -r requirements.txt
+
+echo "finished"
